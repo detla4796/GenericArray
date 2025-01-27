d4e5419eed4977b7566b5bc2a6115b487368abe1
Danil Ushakov <danilusakov16@gmail.com>
2025-01-27 21:23:42 +0500

    First commit

---
+++ GenericArray/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericArray<int> array = new GenericArray<int>();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            PrintElement(array, 0);
+            PrintElement(array, 1);
+            PrintElement(array, 2);
+
+            Console.WriteLine("We are removing element at index 1");
+            array.Remove(1);
+
+            PrintElement(array, 0);
+            PrintElement(array, 1);
+        }
+        static void PrintElement<T>(GenericArray<T> array, int index)
+        {
+            try
+            {
+                Console.WriteLine($"Element at index {index}: {array.GetElement(index)}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    public class GenericArray<T>
+    {
+        private List<T> items;
+        public GenericArray()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public void Remove(int ind)
+        {
+            if (ind >= 0 && ind < items.Count)
+            {
+                items.RemoveAt(ind);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+        }
+        public T GetElement(int ind)
+        {
+            if (ind >= 0 && ind < items.Count)
+            {
+                return items[ind];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Invalid index");
+            }
+        }
+    }
+}
---
+++ GenericArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("GenericArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenericArray")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e969bd78-2ff7-454c-b2d7-9a50474e292e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
